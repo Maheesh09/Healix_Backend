cbcffd7365227ee7aa171357f79835c51bf7d04d
IT24104152 <gayuthdemika@gmail.com>
2026-02-04 23:15:55 +0530

    feat: successfully tested the crud operations for the hospital entity

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
-from app.api.v1.endpoints import doctor
+from app.api.v1.endpoints import doctor, hospital
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 router.include_router(doctor.router)
+router.include_router(hospital.router)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.api.v1.endpoints import doctor
+from app.api.v1.endpoints import doctor, hospital
 
 app = FastAPI(title="Healix Backend API")
 
 app.include_router(doctor.router)
+app.include_router(hospital.router)
