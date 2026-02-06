084f6d5d7726abeb14d0cd3ee51de72dba232562
M. Maheesha Pramuditha <pramudithamaheesha@gmail.com>
2026-02-06 22:59:39 +0530

    Remove commented settings code from config.py
    Removed commented-out code for settings configuration.

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,1 @@
 
-# import os
-
-# class Settings:
-#     PROJECT_NAME: str = "Healix Backend"
-#     SQLALCHEMY_DATABASE_URI: str = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
-
-# settings = Settings()
